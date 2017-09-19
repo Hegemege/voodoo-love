bf46cb5f64d767aa156ea341754a6119dd890f7b
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2017-09-19 18:30:25 +0300

    updated click particles. Where is my pizza!

---
+++ Assets/Scripts/DestroyAfterDelay.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyAfterDelay : MonoBehaviour
+{
+    public float Seconds;
+
+    void Start()
+    {
+        StartCoroutine(DestroyAfterSeconds(Seconds));
+    }
+
+    IEnumerator DestroyAfterSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Destroy(gameObject);
+    }
+}
