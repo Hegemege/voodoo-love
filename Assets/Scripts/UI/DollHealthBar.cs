4a0d96b8425f6a457c0f488d16a06f73ac1526ba
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2017-09-19 15:57:29 +0300

    health bar to main scene

---
+++ Assets/Scripts/UI/DollHealthBar.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class DollHealthBar : MonoBehaviour
+{
+    Slider slider;
+
+    void Awake()
+    {
+        slider = GetComponent<Slider>();
+    }
+
+    void Start()
+    {
+        slider.maxValue = GameController.instance.CurrentDoll.MaxLove;
+    }
+
+    void Update()
+    {
+        slider.value = GameController.instance.CurrentDoll.Love;
+    }
+}
