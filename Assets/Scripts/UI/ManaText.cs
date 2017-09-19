e1623ffd182a81fc234acacfebbf691461a05296
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2017-09-19 14:47:48 +0300

    mana text

---
+++ Assets/Scripts/UI/ManaText.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class ManaText : MonoBehaviour
+{
+    Text manaText;
+
+    void Awake()
+    {
+        manaText = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        manaText.text = FormatMana(GameController.instance.Mana);
+    }
+
+    static string FormatMana(float mana)
+    {
+        if (mana < 1000) return mana.ToString("2F");
+        if (mana < 1000000) return (mana / 1000).ToString("F2") + " K";
+        if (mana < 1000000000) return (mana / 1000000).ToString("F2") + " M";
+        return (mana / 1000000000).ToString("F2") + " B";
+    }
+}
