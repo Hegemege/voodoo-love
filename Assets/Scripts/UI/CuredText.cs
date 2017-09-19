03f3ce4c29ca495d81c82c1be2e83bd620c927d6
Joakim Isaksson <joakim.ove.isaksson@gmail.com>
2017-09-19 15:03:19 +0300

    curec doll count text

---
+++ Assets/Scripts/UI/CuredText.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Slider))]
+public class CuredText : MonoBehaviour
+{
+    Text curedText;
+
+    void Awake()
+    {
+        curedText = GetComponent<Text>();
+    }
+
+    void Update()
+    {
+        curedText.text = GameController.instance.CuredDolls.ToString();
+    }
+}
